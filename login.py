c867e9a38efe5fb265d2e233ed213ce034d0a611
shresthabatsal <149764196+shresthabatsal@users.noreply.github.com>
2024-02-01 11:18:26 +0545

    Update login.py

--- login.py
+++ login.py
@@ -5,58 +5,68 @@ import tkinter as tk
 from tkinter import ttk
 
 root = Tk()
-root.geometry('1920x1080')
-root.title('Liqour')
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.geometry(f"{screen_width}x{screen_height}")
+root.title('Library')
 root.configure(bg='#EEEBEB')
 root.state('zoomed')
+root.resizable(False, False)
 
 box = tk.Frame(root, width=500, height=600, bg='#FFFFFF')
 box.place(relx=0.5, rely=0.5, anchor='center')
 
 welcome = Label(root, text='WELCOME', font=('Montserrat Black', 30), bg='#FFFFFF')
-welcome.place(relx=0.5, y=150, anchor='center')
+welcome.place(relx=0.5, rely=0.2, anchor='center')
 
-email = Label(box, text='Email', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-email.place(relx=0.125, y=170)
+email = Label(root, text='Email', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+email.place(relx=0.365, rely=0.3)
 email_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd')
-email_entry.place(relx=0.5, y=260, anchor='center')
+email_entry.place(relx=0.5, rely=0.35, anchor='center')
 
 def password_visibility():
     if password_entry.cget('show') == '':
         password_entry.config(show='*')
-        hide_button.config(text='Show', font=('Montserrat Light', 10))
+        show_hide.config(image=show_icon)
     else:
         password_entry.config(show='')
-        hide_button.config(text='Hide', font=('Montserrat Light', 10))
+        show_hide.config(image=hide_icon)
+        
 
-password = Label(box, text='Password', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-password.place(relx=0.125, y=270)
+password = Label(root, text='Password', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+password.place(relx=0.365, rely=0.4)
 password_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd', show='*')
-password_entry.place(relx=0.5, y=360, anchor='center')
+password_entry.place(relx=0.5, rely=0.45, anchor='center')
 
-hide_button = tk.Button(root, text='Show', font=('Montserrat Light', 10), height=1, width= 4, command = password_visibility, bg='#FFFFFF')
-hide_button.place(relx=0.603, y=345)
+show_icon = PhotoImage(file='show_small.png')
+hide_icon = PhotoImage(file='hide_small.png')
+show_hide = Button(root,image=show_icon, width=25, command=password_visibility, fg='white', bg='#FFFFFF')
+show_hide.place(relx=0.622, rely=0.45, anchor='center')
 
-role = Label(box, text='Role', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-role.place(relx=0.125, y=370)
+role = Label(box, text='Role', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+role.place(relx=0.125, rely=0.5)
 clicked = tk.StringVar()
 
 role_menu = ttk.Combobox(root, textvariable=clicked, values=['Admin', 'User'], font=('Montserrat Light', 16), width=25, state='readonly')
-role_menu.place(relx=0.5, y=460, anchor='center')
+role_menu.place(relx=0.5, rely=0.55, anchor='center')
 
 def open_register():
+    root.destroy()
     import register
     register.open_register_window()
 
-login_button = Button(root, width=26, text='Login', font=('Montserrat', 12), fg='#FFFFFF', bg='black')
-login_button.place(relx=0.5, y=560, anchor='center')
+def open_dashboard():
+    root.destroy()
+    #import dashboard
+    #dashboard.open_register_window()
+
+login_button = Button(root, width=26, text='Login', font=('Montserrat', 10), command=open_dashboard, fg='#FFFFFF', bg='black')
+login_button.place(relx=0.5, rely=0.7, anchor='center')
 
 sign_up = Label(root, text="Don't have an accout?", font=('Montserrat', 10, 'italic'), fg='#413F3F', bg='#FFFFFF')
-sign_up.place(relx=0.47, y=600, anchor='center')
-create_one = Label(root, text="Create one.", font=('Montserrat', 10, 'bold'), fg='#413F3F', bg='#FFFFFF')
-create_one.place(relx=0.555, y=600, anchor='center')
+sign_up.place(relx=0.47, rely=0.75, anchor='center')
 
 register_button = Button(root, text='Create one.', command=open_register, font=('Montserrat', 10, 'bold'), fg='black', bg='#FFFFFF')
-register_button.place(relx=0.557, y=600, anchor='center')
+register_button.place(relx=0.557, rely=0.75, anchor='center')
 
 root.mainloop()

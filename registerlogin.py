a4e488743f483afe85ddfab9ccef3afcccaad3f9
shresthabatsal <149764196+shresthabatsal@users.noreply.github.com>
2024-02-01 11:43:44 +0545

    Update registerlogin.py

--- registerlogin.py
+++ registerlogin.py
@@ -3,128 +3,84 @@ import tkinter.messagebox as MessageBox
 import mysql.connector as mysql
 import tkinter as tk
 from tkinter import ttk
-from validate_email_address import validate_email
-
 
 root = Tk()
-root.geometry('1920x1080')
-root.title('Liqour')
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.geometry(f"{screen_width}x{screen_height}")
+root.title('Library')
 root.configure(bg='#EEEBEB')
 root.state('zoomed')
-
-
-def create_account():
-    print("Inside create_account")
-
-    email_content = email_entry.get()
-    if validate_email(email_content, verify= True):
-        print("valid email")
-    else:
-        MessageBox.showerror("Error","Enter a valid email address")
-
-    first_name = first_name_entry.get()
-    second_name = second_name_entry.get()
-    email= email_entry.get()
-    role =role_menu.get()
-    password = password_entry.get()
-    confirm_password=confirm_password_entry.get()
-    
-    if not email or not password or not role:
-        MessageBox.showerror('ERROR', "Please fill all the gaps")
-        return
-    
-    try:
-        connection = mysql.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database ="project"
-        )
-        cursor= connection.cursor()
-
-        query = "INSERT INTO users(`first_name`, `second_name`, `email`, `role`, `password`, `confirm_password`) VALUES (%s,%s,%s,%s,%s,%s)"
-        data= (first_name,second_name,email, role, password,confirm_password)
-        cursor.execute(query,data)
-
-        connection.commit()
-        MessageBox.showinfo("Done", "Data added to the databse successfully")
-    except Exception as e:
-        MessageBox.showerror("Error",f"Error:{e}")
-
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-
-
-    
-             
+root.resizable(False, False)
 
 box = tk.Frame(root, width=500, height=600, bg='#FFFFFF')
 box.place(relx=0.5, rely=0.5, anchor='center')
 
 register = Label(root, text='REGISTER', font=('Montserrat Black', 30), bg='#FFFFFF')
-register.place(relx=0.5, y=90, anchor='center')
+register.place(relx=0.5, rely=0.15, anchor='center')
 
-first_name = Label(root, text='First name', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-first_name.place(relx=0.355, y=140)
+first_name = Label(root, text='First name', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+first_name.place(relx=0.365, rely=0.25)
 first_name_entry = Entry(root, width=10, font=('Montserrat Light', 16), bg='#fdfdfd')
-first_name_entry.place(relx=0.409, y=180, anchor='center')
+first_name_entry.place(relx=0.419, rely=0.3, anchor='center')
 
-second_name = Label(root, text='Second name', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-second_name.place(relx=0.515, y=140)
+second_name = Label(root, text='Second name', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+second_name.place(relx=0.525, rely=0.25)
 second_name_entry = Entry(root, width=10, font=('Montserrat Light', 16), bg='#fdfdfd')
-second_name_entry.place(relx=0.57, y=180, anchor='center')
+second_name_entry.place(relx=0.58,rely=0.3, anchor='center')
 
-email = Label(root, text='Email', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-email.place(relx=0.355, y=230)
+email = Label(root, text='Email', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+email.place(relx=0.365, rely=0.35)
 email_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd')
-email_entry.place(relx=0.49, y=270, anchor='center')
- 
+email_entry.place(relx=0.5, rely=0.4, anchor='center')
 
-
-role = Label(root, text='Role', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-role.place(relx=0.355, y=320)
+role = Label(root, text='Role', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+role.place(relx=0.365, rely=0.45)
 clicked = tk.StringVar()
 
 role_menu = ttk.Combobox(root, textvariable=clicked, values=['Admin', 'User'], font=('Montserrat Light', 16), width=25, state='readonly')
-role_menu.place(relx=0.49, y=360, anchor='center')
+role_menu.place(relx=0.5, rely=0.5, anchor='center')
 
 def password_visibility():
     if password_entry.cget('show') == '':
         password_entry.config(show='*')
-        hide_button.config(text='Show', font=('Montserrat Light', 10))
+        show_hide.config(image=show_icon)
     else:
         password_entry.config(show='')
-        hide_button.config(text='Hide', font=('Montserrat Light', 10))
+        show_hide.config(image=hide_icon)
 
-password = Label(root, text='Password', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-password.place(relx=0.355, y=410)
-password_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd')
-password_entry.place(relx=0.49, y=450, anchor='center')
+password = Label(root, text='Password', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+password.place(relx=0.365, rely=0.55)
+password_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd', show='*')
+password_entry.place(relx=0.5, rely=0.6, anchor='center')
 
-hide_button = tk.Button(root, text='Hide', font=('Montserrat Light', 10), height=1, width= 4, command = password_visibility, bg='#FFFFFF')
-hide_button.place(relx=0.593, y=435)
+show_icon = PhotoImage(file='show_small.png')
+hide_icon = PhotoImage(file='hide_small.png')
+show_hide = Button(root,image=show_icon, width=25, command=password_visibility, fg='white', bg='#FFFFFF')
+show_hide.place(relx=0.622, rely=0.6, anchor='center')
 
 def confirm_password_visibility():
     if confirm_password_entry.cget('show') == '':
         confirm_password_entry.config(show='*')
-        hide_button2.config(text='Show', font=('Montserrat Light', 10))
+        confirm_show_hide.config(confirm_show_hide.config(image=show_icon))
     else:
         confirm_password_entry.config(show='')
-        hide_button2.config(text='Hide', font=('Montserrat Light', 10))
+        confirm_show_hide.config(confirm_show_hide.config(image=hide_icon))
 
-confirm_password = Label(root, text='Confirm password', font=('Montserrat', 12), fg='#413F3F', bg='#FFFFFF')
-confirm_password.place(relx=0.355, y=500)
-confirm_password_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd')
-confirm_password_entry.place(relx=0.49, y=540, anchor='center')
+confirm_password = Label(root, text='Confirm password', font=('Montserrat', 10), fg='#413F3F', bg='#FFFFFF')
+confirm_password.place(relx=0.365, rely=0.65)
+confirm_password_entry = Entry(root, width=26, font=('Montserrat Light', 16), bg='#fdfdfd', show='*')
+confirm_password_entry.place(relx=0.5, rely=0.7, anchor='center')
 
-hide_button2 = tk.Button(root, text='Hide', font=('Montserrat Light', 10), height=1, width= 4, command = confirm_password_visibility, bg='#FFFFFF')
-hide_button2.place(relx=0.593, y=525)
+confirm_show_hide = Button(root,image=show_icon, width=25, command=confirm_password_visibility, fg='white', bg='#FFFFFF')
+confirm_show_hide.place(relx=0.622, rely=0.7, anchor='center')
 
-login_button = Button(root, width=26, text='Create my account', command= create_account ,font=('Montserrat', 12), fg='#FFFFFF', bg='black')
-login_button.place(relx=0.49, y=610, anchor='center')
+def open_login():
+    root.destroy()
+    import login
+    login.open_register_window()
 
-
+login_button = Button(root, width=26, text='Create my account', font=('Montserrat', 10), command=open_login, fg='#FFFFFF', bg='black')
+login_button.place(relx=0.5, rely=0.8, anchor='center')
 
 root.mainloop()
